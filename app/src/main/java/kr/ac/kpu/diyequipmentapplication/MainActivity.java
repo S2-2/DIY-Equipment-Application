8072965b64ed3b31383183b7840ace3d598dda6d
Nangnis <misoppiyak@gmail.com>
2022-02-07 22:14:56 +0900

    commit test

---
+++ app/src/main/java/kr/ac/kpu/diyequipmentapplication/MainActivity.java
@@ -0,0 +1,17 @@
+package kr.ac.kpu.diyequipmentapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        adf
+    }
+}
