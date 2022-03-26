8d3c446942392db48fdcf9c6bf7be7850b439f26
Nangnis <misoppiyak@gmail.com>
2022-03-26 17:22:08 +0900

    [xml] findId, checkId, findPwd, changePwd

---
+++ app/src/main/java/kr/ac/kpu/diyequipmentapplication/FindPwdActivity.java
@@ -0,0 +1,13 @@
+package kr.ac.kpu.diyequipmentapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FindPwdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_pwd);
+    }
+}
