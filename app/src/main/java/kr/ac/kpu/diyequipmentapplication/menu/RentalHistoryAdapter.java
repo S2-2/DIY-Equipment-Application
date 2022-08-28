21205b6d50f46e08352efff1f57e988315d82646
YunHa <yhhwang0283@gmail.com>
2022-08-28 10:34:14 +0900

    220828_회원가입, 장비등록, 채팅, 거래일정(대여비), 거래, 거래내역, 반납 오류 수정!

--- app/src/main/java/kr/ac/kpu/diyequipmentapplication/menu/RentalHistoryAdapter.java
+++ app/src/main/java/kr/ac/kpu/diyequipmentapplication/menu/RentalHistoryAdapter.java
@@ -4,6 +4,7 @@ import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -34,6 +35,8 @@ import java.util.List;
 import java.util.Map;
 
 import kr.ac.kpu.diyequipmentapplication.R;
+import kr.ac.kpu.diyequipmentapplication.RentalHistoryRecyclerviewActivity;
+import kr.ac.kpu.diyequipmentapplication.ScheduleReturnActivity;
 import kr.ac.kpu.diyequipmentapplication.chat.TransactionDTO;
 
 public class RentalHistoryAdapter extends RecyclerView.Adapter<RentalHistoryAdapter.ViewHolder> {
@@ -66,6 +69,20 @@ public class RentalHistoryAdapter extends RecyclerView.Adapter<RentalHistoryAdap
         String imageUri = null;
         imageUri=transactionDTO.gettImgView();
         Picasso.get().load(imageUri).into(holder.imgViewRental);
+
+        //
+        if(transactionDTO.gettUserEmail().equals(holder.transactionFirebaseAuth.getCurrentUser().getEmail())) {
+            holder.btnReturnCheck.setEnabled(false);
+            holder.btnReturnCheck.setVisibility(View.GONE);
+        } else{
+            holder.btnTreturn.setEnabled(false);
+            holder.btnTreturn.setVisibility(View.GONE);
+        }
+
+        if(transactionDTO.gettTransactionCondition().equals("대여")) {
+            holder.btnMyEquipmentDelete.setEnabled(false);
+            holder.btnMyEquipmentDelete.setVisibility(View.GONE);
+        }
     }
 
     @Override
@@ -89,6 +106,16 @@ public class RentalHistoryAdapter extends RecyclerView.Adapter<RentalHistoryAdap
         Button btnTreturn;
         String getTransactionDBId;
         String getTransactionChatNum;
+        Button btnReturnCheck;
+        Button btnMyEquipmentDelete;
+        String getRentalId;
+
+        Dialog returnDetailDialog;  //반납 상세페이지 커스텀 다이얼로그
+        ImageView imgViewT1;
+        TextView tvTcategory1, tvTmodelName1, tvTuserName1, tvTrentalType1, tvTrentalDate1, tvTrentalCost1,
+                tvTtransactionDate1, tvTtransactionTime1, tvTtransactionLocation1;
+
+        Button btnTreturn1;
 
         //ViewHolder 클래스 생성자
         public ViewHolder(@NonNull View itemView) {
@@ -102,6 +129,8 @@ public class RentalHistoryAdapter extends RecyclerView.Adapter<RentalHistoryAdap
             tvRentalDate = itemView.findViewById(R.id.rentalHistoryRecyclerview_tv_rentalDate);
             tvRentalCost = itemView.findViewById(R.id.rentalHistoryRecyclerview_tv_rentalCost);
             tvCondition = itemView.findViewById(R.id.rentalHistoryRecyclerview_tv_condition);
+            btnReturnCheck = itemView.findViewById(R.id.rentalHistory_btn_return);
+            btnMyEquipmentDelete = itemView.findViewById(R.id.rentalHistory_btn_delete);
 
             //거래 기능 관련 참조
             transactionFirebaseAuth = FirebaseAuth.getInstance();
@@ -127,6 +156,24 @@ public class RentalHistoryAdapter extends RecyclerView.Adapter<RentalHistoryAdap
             tvTtransactionTime = transactionDialog.findViewById(R.id.transaction_tv_transactionTime);
             tvTtransactionLocation = transactionDialog.findViewById(R.id.transaction_tv_transactionLocation);
             btnTreturn = transactionDialog.findViewById(R.id.transaction_btn_return);
+
+            //반납 기능 관련
+            returnDetailDialog = new Dialog(context);
+            returnDetailDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+            returnDetailDialog.setContentView(R.layout.returndetail_window_item);
+
+            imgViewT1 = returnDetailDialog.findViewById(R.id.transaction_imgView);
+            tvTcategory1 = returnDetailDialog.findViewById(R.id.transaction_tv_category);
+            tvTmodelName1 = returnDetailDialog.findViewById(R.id.transaction_tv_modelName);
+            tvTuserName1 = returnDetailDialog.findViewById(R.id.transaction_tv_userName);
+            tvTrentalType1 = returnDetailDialog.findViewById(R.id.transaction_tv_rentalType);
+            tvTrentalDate1 = returnDetailDialog.findViewById(R.id.transaction_tv_rentalDate);
+            tvTrentalCost1 = returnDetailDialog.findViewById(R.id.transaction_tv_rentalCost);
+
+            tvTtransactionDate1 = returnDetailDialog.findViewById(R.id.transaction_tv_transactionDate);
+            tvTtransactionTime1 = returnDetailDialog.findViewById(R.id.transaction_tv_transactionTime);
+            tvTtransactionLocation1 = returnDetailDialog.findViewById(R.id.transaction_tv_transactionLocation);
+            btnTreturn1 = returnDetailDialog.findViewById(R.id.transaction_btn_return);
 
 
 
@@ -185,53 +232,141 @@ public class RentalHistoryAdapter extends RecyclerView.Adapter<RentalHistoryAdap
                         btnTreturn.setOnClickListener(new View.OnClickListener() {
                             @Override
                             public void onClick(View view) {
+                                Intent intent = new Intent(context, ScheduleReturnActivity.class).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                intent.putExtra("RentalHistoryId",getTransactionDBId);
+                                context.startActivity(intent);
+                            }
+                        });
+
+                        // 등록된 장비 삭제
+                        btnMyEquipmentDelete.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                //Toast.makeText(context, title + ": 로그아웃", Toast.LENGTH_SHORT).show();
                                 AlertDialog.Builder dlg = new AlertDialog.Builder(context);
-                                dlg.setTitle("DIY_반납");
-                                dlg.setMessage("공구 반납 진행을 하시겠습니까?");
+                                dlg.setTitle("반납 장비 삭제");
+                                dlg.setMessage("반납 장비 삭제하시겠습니까?");
                                 dlg.setIcon(R.mipmap.ic_launcher);
 
                                 dlg.setPositiveButton("예", new DialogInterface.OnClickListener() {
                                     @Override
                                     public void onClick(DialogInterface dialogInterface, int i) {
-                                        final String transactionCondition = "반납";
-                                        transactionDTO.settTransactionCondition(transactionCondition);
 
-                                        Map<String, Object> rentalHistoryUpdate = new HashMap<String, Object>();
-                                        rentalHistoryUpdate.put("tTransactionCondition", transactionDTO.gettTransactionCondition());
+                                        transactionFirebaseFirestore.collection("DIY_Schedule")
+                                                .document(transactionDTO.gettScheduleId())
+                                                .get()
+                                                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                                    @Override
+                                                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                                        if(task.isSuccessful()) {
+                                                            getRentalId = task.getResult().get("sCollectionId").toString().trim();
 
-                                        if (!(transactionDTO.gettTransactionCondition().equals(transactionCondition))) {
-                                            transactionFirebaseFirestore.collection("DIY_Transaction")
-                                                    .document(getTransactionDBId)
-                                                    .update(rentalHistoryUpdate)
-                                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
-                                                        @Override
-                                                        public void onSuccess(Void aVoid) {
-                                                            Log.d("공구 반납 업데이트 성공!", "DocumentSnapshot successfully update!");
+                                                            transactionFirebaseFirestore.collection("DIY_Equipment_Rental")
+                                                                    .document(getRentalId)
+                                                                    .delete()
+                                                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                                        @Override
+                                                                        public void onSuccess(Void aVoid) {
+                                                                            Log.d("반납 장비 삭제 성공!", "DocumentSnapshot successfully deleted!");
+                                                                        }
+                                                                    })
+                                                                    .addOnFailureListener(new OnFailureListener() {
+                                                                        @Override
+                                                                        public void onFailure(@NonNull Exception e) {
+                                                                            Log.w("반납 장비 삭제 실패", "Error deleting document", e);
+                                                                        }
+                                                                    });
                                                         }
-                                                    })
-                                                    .addOnFailureListener(new OnFailureListener() {
-                                                        @Override
-                                                        public void onFailure(@NonNull Exception e) {
-                                                            Log.w("공구 반납 업데이트 실패", "Error updating document", e);
-                                                        }
-                                                    });
+                                                    }
+                                                });
 
-                                            Toast.makeText(context, "공구 반납 완료 되었습니다!", Toast.LENGTH_SHORT).show();
 
-                                        } else {
-                                            Toast.makeText(context, "반납 완료된 공구입니다!", Toast.LENGTH_SHORT).show();
-                                        }
-                                        transactionDialog.dismiss();
+                                        Intent intent = new Intent(context, RentalHistoryRecyclerviewActivity.class).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                        context.startActivity(intent);
                                     }
                                 });
 
                                 dlg.setNegativeButton("아니오", new DialogInterface.OnClickListener() {
                                     @Override
                                     public void onClick(DialogInterface dialogInterface, int i) {
-                                        Toast.makeText(context, "공구 반납 취소 되었습니다!", Toast.LENGTH_SHORT).show();
+                                        Toast.makeText(context, " 취소되었습니다!", Toast.LENGTH_SHORT).show();
                                     }
                                 });
                                 dlg.show();
+                            }
+                        });
+
+                        btnReturnCheck.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Picasso.get().load(transactionDTO.gettImgView()).into(imgViewT1);
+                                tvTcategory1.setText(transactionDTO.gettCategory());
+                                tvTmodelName1.setText(transactionDTO.gettModelName());
+                                tvTuserName1.setText(transactionDTO.gettUserName());
+                                tvTrentalType1.setText(transactionDTO.gettRentalType());
+                                tvTrentalDate1.setText(transactionDTO.gettRentalDate());
+                                tvTrentalCost1.setText(transactionDTO.gettRentalCost());
+                                tvTtransactionDate1.setText(transactionDTO.gettTransactionDate());
+                                tvTtransactionTime1.setText(transactionDTO.gettTransactionTime());
+                                tvTtransactionLocation1.setText(transactionDTO.gettTransactionLocation());
+                                returnDetailDialog.show();
+
+                                //반납 상세 페이지 확인 버튼 클릭 이벤트
+                                btnTreturn1.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View view) {
+                                        AlertDialog.Builder dlg = new AlertDialog.Builder(context);
+                                        dlg.setTitle("DIY_반납");
+                                        dlg.setMessage("공구 반납 진행을 하시겠습니까?");
+                                        dlg.setIcon(R.mipmap.ic_launcher);
+
+                                        dlg.setPositiveButton("예", new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialogInterface, int i) {
+                                                final String transactionCondition = "반납";
+                                                //transactionDTO.settTransactionCondition(transactionCondition);
+
+                                                Map<String, Object> rentalHistoryUpdate = new HashMap<String, Object>();
+
+
+                                                if (!(transactionDTO.gettTransactionCondition().equals(transactionCondition))) {
+                                                    transactionDTO.settTransactionCondition(transactionCondition);
+                                                    rentalHistoryUpdate.put("tTransactionCondition", transactionDTO.gettTransactionCondition());
+
+                                                    transactionFirebaseFirestore.collection("DIY_Transaction")
+                                                            .document(getTransactionDBId)
+                                                            .update(rentalHistoryUpdate)
+                                                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                                @Override
+                                                                public void onSuccess(Void aVoid) {
+                                                                    Log.d("공구 반납 업데이트 성공!", "DocumentSnapshot successfully update!");
+                                                                    tvCondition.setText(transactionDTO.gettTransactionCondition());
+                                                                }
+                                                            })
+                                                            .addOnFailureListener(new OnFailureListener() {
+                                                                @Override
+                                                                public void onFailure(@NonNull Exception e) {
+                                                                    Log.w("공구 반납 업데이트 실패", "Error updating document", e);
+                                                                }
+                                                            });
+
+                                                    Toast.makeText(context, "공구 반납 완료 되었습니다!", Toast.LENGTH_SHORT).show();
+                                                } else {
+                                                    Toast.makeText(context, "반납 완료된 공구입니다!", Toast.LENGTH_SHORT).show();
+                                                }
+                                                returnDetailDialog.dismiss();
+                                            }
+                                        });
+
+                                        dlg.setNegativeButton("아니오", new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialogInterface, int i) {
+                                                Toast.makeText(context, "공구 반납 취소 되었습니다!", Toast.LENGTH_SHORT).show();
+                                            }
+                                        });
+                                        dlg.show();
+                                    }
+                                });
                             }
                         });
                     }
