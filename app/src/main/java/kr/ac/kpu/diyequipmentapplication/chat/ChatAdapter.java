09aeba011995b6f31b734f2e2431d3ffbe12bf1b
승섭kim <mcar120@fgb.ai>
2022-08-29 18:04:04 +0900

    채팅목록, 채팅 프로필이미지 적용

--- app/src/main/java/kr/ac/kpu/diyequipmentapplication/chat/ChatAdapter.java
+++ app/src/main/java/kr/ac/kpu/diyequipmentapplication/chat/ChatAdapter.java
@@ -1,12 +1,22 @@
 package kr.ac.kpu.diyequipmentapplication.chat;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -17,6 +27,9 @@ public class ChatAdapter extends BaseAdapter {
     ArrayList<ChatDTO> chattingModelList;
     LayoutInflater inflater;
     FirebaseAuth userAuth;
+    FirebaseFirestore chatFirestore = null;
+    String getProfileUrl = null;
+    String profile = null;
 
     public ChatAdapter(ArrayList<ChatDTO> chattingModelList, LayoutInflater inflater) {
         this.chattingModelList = chattingModelList;
@@ -54,6 +67,9 @@ public class ChatAdapter extends BaseAdapter {
             itemView = inflater.inflate(R.layout.activity_chat_otherform,viewGroup,false);
         }
 
+        // 프로필 값 설정
+        ImageView ivProfile = itemView.findViewById(R.id.chat_iv);
+
         // itemView 값들 설정 (이름, 메시지, 시간)
         TextView tvName = itemView.findViewById(R.id.chat_tv_name);
         TextView tvMsg = itemView.findViewById(R.id.chat_tv_msg);
@@ -62,6 +78,31 @@ public class ChatAdapter extends BaseAdapter {
         tvName.setText(chat.getUserNickname());
         tvMsg.setText(chat.getUserMsg());
         tvTime.setText(chat.getTimestamp());
+
+        chatFirestore = FirebaseFirestore.getInstance();
+
+        if(userAuth.getCurrentUser().getEmail().equals(chat.getUserEmail())){
+            profile = chat.getUserEmail();
+        } else if(userAuth.getCurrentUser().getEmail().equals(chat.getOtherEmail())){
+            profile = chat.getUserEmail();
+        }
+
+        chatFirestore.collection("DIY_Profile")
+                .whereEqualTo("profileEmail", profile)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful()){
+                            for (QueryDocumentSnapshot queryDocumentSnapshot: task.getResult()){
+                                Log.e("chatprofile-mine", userAuth.getCurrentUser().getEmail().toString());
+                                Log.e("chatprofile-other",profile);
+                                getProfileUrl = queryDocumentSnapshot.get("profileImage").toString().trim();
+                                Picasso.get().load(getProfileUrl).into(ivProfile);
+                            }
+                        }
+                    }
+                });
 
         return itemView;
     }
